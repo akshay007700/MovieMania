6a07e7a4e5492e7f198c6f08489226f0b2a0628f
Akshay <ak0077003@gmail.com>
2025-09-18 02:17:57 +0530

    Update MovieMania site

---
+++ scipt.js
@@ -0,0 +1,9 @@
+// Mobile Menu Toggle
+const menuToggle = document.getElementById("menuToggle");
+const navLinks = document.getElementById("navLinks");
+
+if (menuToggle) {
+  menuToggle.addEventListener("click", () => {
+    navLinks.classList.toggle("active");
+  });
+}
